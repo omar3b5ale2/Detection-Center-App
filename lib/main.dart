13d98c3830abcf0d109c742bdba3a73476bdf042
OM AR <om1446587@gmail.com>
2024-12-03 20:50:40 +0200

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pyramakerz_assement/core/config/themes/cubit/theme_cubit.dart';
 import 'core/config/router/app_route.dart';
-import 'core/config/themes/app_themes.dart';
 import 'core/constants/app_constants.dart';
 import 'core/constants/app_texts.dart';
 import 'core/handler/permission_manager.dart';
 import 'feature/Intro/view-model/cubit/intro_cubit.dart';
 import 'feature/splash/view-model/cubit/splash_cubit.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,25 +30,28 @@ class DetectionCenterApp extends StatelessWidget {
       providers: [
         BlocProvider<SplashCubit>(create: (context) => SplashCubit()),
         BlocProvider<IntroCubit>(create: (context) => IntroCubit()),
+        BlocProvider<ThemeCubit>(create: (context) => ThemeCubit()),
       ],
       child: ScreenUtilInit(
         designSize:
             Size(AppConstants.figmaDesignWidth, AppConstants.figmaDesignHeight),
         minTextAdapt: true,
         splitScreenMode: true,
-        child: MaterialApp(
-          title: AppTexts.appName,
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          locale: Locale('en', 'US'), // Default locale
-          supportedLocales: [
-            Locale('en', 'US'),
-            Locale('ar', 'SA'), // Arabic locale
-          ],
-          initialRoute: AppRoutes.splash,
-          onGenerateRoute: AppRoutes.generateRoute,
-          debugShowCheckedModeBanner: false,
-        ),
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (BuildContext context, state) {
+          return MaterialApp(
+            title: AppTexts.appName,
+            theme: state.themeData,
+            locale: Locale('en', 'US'), // Default locale
+            supportedLocales: [
+              Locale('en', 'US'),
+              Locale('ar', 'SA'), // Arabic locale
+            ],
+            initialRoute: AppRoutes.splash,
+            onGenerateRoute: AppRoutes.generateRoute,
+            debugShowCheckedModeBanner: false,
+          );
+        }),
       ),
     );
   }
