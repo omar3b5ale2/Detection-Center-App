fd01fceed0badd75950ed675e1903a6ab46a3b8a
OM AR <om1446587@gmail.com>
2024-12-03 17:38:54 +0200

    finished

--- lib/core/config/themes/app_themes.dart
+++ lib/core/config/themes/app_themes.dart
@@ -5,17 +5,12 @@ class AppTheme {
   static final lightTheme = ThemeData(
     brightness: Brightness.light,
     primaryColor: AppColors.primary,
-    colorScheme: ColorScheme.fromSwatch()
-        .copyWith(secondary: AppColors.accent)
-        .copyWith(surface: AppColors.background),
+
   );
 
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: AppColors.accent,
-    colorScheme: ColorScheme.fromSwatch()
-        .copyWith(secondary: AppColors.primary)
-        .copyWith(surface: AppColors.backGroundBlack),
-    // ... other theme properties
+
   );
 }
